28f1a0adb88be8317714616ba7c2d2420fdb5bb2
Daniele Polencic <daniele.polencic@which.co.uk>
2013-07-02 16:21:44 +0100

    removed gmail gem, using sendgrid

--- spammer.rb
+++ spammer.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'liquid'
 require 'liquid_blocks'
+require 'pony'
+
+
+
+
 require './lib/spammer.rb'
 
 get "/" do
@@ -26,12 +31,23 @@ post "/spam" do
   for receiver in receivers do
     html = text = liquid "building the template", :locals => receiver
     begin
-      spammer.spam({
-        :subject => params[:subject],
-        :to => receiver[:email],
-        :text => text,
-        :html => html
-      })
+      Pony.mail(
+        :from => "#{ params[:name] } <#{ params[:username] }>",
+        :to => "#{ receiver[:email] }"
+        :subject => "#{ params[:subject] }"
+        :body => text,
+        :html_body => html,
+        :port => '587',
+        :via => :smtp,
+        :via_options => {
+          :address => 'smtp.sendgrid.net',
+          :port => '587',
+          :domain => 'heroku.com',
+          :user_name => ENV['SENDGRID_USERNAME'],
+          :password => ENV['SENDGRID_PASSWORD'],
+          :authentication => :plain,
+          :enable_starttls_auto => true
+        })
       log.push "Processing #{ receiver[:email] }\n"
     rescue
       log.push "Error processing #{ receiver[:email] }\n"
